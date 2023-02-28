f44cd4dbd681239e52e5c619969ca3d06f0a1923
Ryan Fuhrman <rsfuhrman10@gmail.com>
2023-02-28 10:40:49 -0500

    Add css for Card component

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import "./Card.css";
 
 const Card = ({ cardInfo }) => {
   return (
-    <ul>
-      {cardInfo.map((card) => {
-        return <li key={card.id}>{card.name}</li>;
+    <ul className="card-ul">
+      {cardInfo.map(({ id, name, img }) => {
+        return (
+          <li key={id} className="card-li">
+            <h1 className="card-h1">{name}</h1>
+            <img src={img} className="card-img" />
+          </li>
+        );
       })}
     </ul>
   );
